2cd5e9a5373e6fa8e964f5b5b2c204c8db489d7b
Vedant <vedantnarwade121@gmail.com>
2022-12-08 11:39:06 +0530

    Initial Commit

---
+++ hi.java
@@ -0,0 +1,43 @@
+// // package com.example.myjavacode;
+
+// // import com.sun.org.apache.xpath.internal.objects.XString;
+
+// // import java.sql.SQLOutput;
+
+// public class hi {
+// // public static void main(String[] args) {S
+// // System.out.println("helo world");
+// // System.out.println("My name is vedant");
+// // }
+// public static void main(String[] args) {
+// String name = "vedant";
+// int a = 19;
+// float command ;
+
+// if (command == 0) {
+// System.out.println("CodeRunner running North");
+// } else if (command == 1) {
+// System.out.println("CodeRunner running South");
+
+// } else if (command == 2) {
+// System.out.println("CodeRunner running West");
+// } else if (command == 3) {
+// System.out.println("CodeRunner is running East");
+// } else if (command > 0 && command >= 4) {
+
+// System.out.println("CodeRunner is confused-don't know where to go ");
+// } else {
+// System.out.println("Unknown command ");
+
+// }
+
+// //
+// // System.out.println("My name is " + name + ",I am " + a + " Year old");
+// // hi("Narwade");
+// // }
+// //
+// // public static void hi(String something) {
+// // System.out.println("Hi my last name is " + something);
+// // }
+// }
+// }
